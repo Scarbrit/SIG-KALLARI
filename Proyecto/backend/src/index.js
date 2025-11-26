60c8f491c39756d1ad0c6832eda19fe1d655ccdc
Scarbrit <scarletcayapa@gmail.com>
2025-11-25 19:46:26 -0500

    Sprint 6 y diagrama de actividades

--- Proyecto/backend/src/index.js
+++ Proyecto/backend/src/index.js
@@ -1,15 +1,26 @@
+import 'dotenv/config'; // 👈 Carga .env al inicio
 import app from './app.js';
+import { createSchemas } from './utils/createSchemas.js';
+import db from './database/database.js';
 
 async function main() {
     try {
-        // Establece relaciones después de importar modelos
-        //setupAssociations();
-        //await sequelize.sync({ force: false });
-        //console.log("Base de datos sincronizada correctamente..");
-        app.listen(4000);
-        console.log("Server is listening on port", 4000);
+        // 1. Crear esquemas en PostgreSQL
+        await createSchemas();
+
+        // 2. Migra modelos a la base de datos
+        await db.sequelize.sync({ alter: false}); // ⚠️ Solo en desarrollo
+        console.log('✅ Base de datos sincronizada.');
+
+        // 3. Levantar servidor
+        const PORT = process.env.PORT || 4000;
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is listening on port ${PORT}`);
+        });
+
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('💥 Unable to start the application:', error.message);
+        process.exit(1);
     }
 }
 
